a730655a56ddf1bf3c87cb89d30d4a15bdd805cd
DevTechProfile <30285858+DevTechProfile@users.noreply.github.com>
2019-05-21 08:24:50 +0200

    initial project setup and .gitignore

---
+++ source/NSuiteX/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NSuiteX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
