cc324cd863b42cade8335536ff0c82ff43834fa1
DecTechProfile <devtechprofile@hotmail.com>
2019-05-30 13:18:49 +0200

    implemented reverse complement, added big test data file

---
+++ source/NSuiteX.Benchmark/Program.cs
@@ -0,0 +1,22 @@
+﻿using NSuiteX.Library.ReverseComplement;
+using System;
+using System.Diagnostics;
+
+namespace NSuiteX.Benchmark
+{
+    class Program
+    {
+        static Stopwatch _reverseComplementSingleStopwatch;
+
+        static void Main(string[] args)
+        {
+            _reverseComplementSingleStopwatch = new Stopwatch();
+            _reverseComplementSingleStopwatch.Start();
+            ReverseComplement.RunMultiThread();
+            _reverseComplementSingleStopwatch.Stop();
+            Console.WriteLine($"Elapsed ms reverse complement: {_reverseComplementSingleStopwatch.ElapsedMilliseconds}");
+
+            Console.Read();
+        }
+    }
+}
---
+++ source/NSuiteX.TestCall/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace NSuiteX.TestCall
+{
+    class Program
+    {
+        static Stopwatch _testAlgorithmStopwatch;
+
+        public static void Main(string[] args)
+        {
+            _testAlgorithmStopwatch = new Stopwatch();
+            _testAlgorithmStopwatch.Start();
+
+            // put test call here
+
+            _testAlgorithmStopwatch.Stop();
+            Console.WriteLine($"Elapsed ms test algorithm: {_testAlgorithmStopwatch.ElapsedMilliseconds}");
+        }
+    }
+}
